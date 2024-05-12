ed538f7faa68e6a2c28da0bc6b2899e53c509ab4
Harun Sheikhali <sufisheikhali@gmail.com>
2024-05-12 12:19:55 -0400

    wip: working on new website design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@ export default {
   theme: {
     fontFamily: {
       satoshi: ["Satoshi", "sans-serif"],
-      "big-shoulders-display": ['"Big Shoulders Display"', "sans-serif"]
+      "big-shoulders-display": ['"Big Shoulders Display"', "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "career-dark": "#0C1827",
+        "career-green": "#5FE668",
+        "career-gray": "#96A3B2",
+        "career-slate": "#272B36",
+        "career-gray-2": "#3C404B",
+        "career-orange": "#FF543D",
+      },
+    },
   },
   plugins: [],
 };
